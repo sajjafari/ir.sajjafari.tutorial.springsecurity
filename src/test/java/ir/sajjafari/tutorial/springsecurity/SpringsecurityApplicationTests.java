c7074d5b28ab17bac0c1990fe0f749fc508242b2
Sajjafari74 <Sajjafari74@gmail.com>
2020-08-11 18:04:31 +0430

    first commit

---
+++ src/test/java/ir/sajjafari/tutorial/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sajjafari.tutorial.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
